96f05a1964f83fe20de50be30ce4a3f542a8480e
Andrii Shekhovtsov <zzzadey@gmail.com>
2023-08-11 10:07:15 +0200

    Add PoC of the explained call feature

--- pymcdm/methods/mcda_method.py
+++ pymcdm/methods/mcda_method.py
@@ -1,39 +1,70 @@
 # Copyright (c) 2020,2022 Andrii Shekhovtsov
 
 from abc import ABC
+import numpy as np
 from ..helpers import rankdata
 
-
 class MCDA_method(ABC):
-    reverse_ranking = True
+    _reverse_ranking = True
 
-    def __call__(self, matrix, weights, types, *args, **kwargs):
-        """Rank alternatives from decision matrix `matrix`, with criteria weights `weights` and criteria types `types`.
+    def __call__(self, matrix, weights, types,
+                 skip_validations=False,
+                 explained_call=False):
+        """Rank alternatives from decision matrix `matrix`, with criteria 
+           weights `weights` and criteria types `types`.
 
-        Parameters
-        ----------
-            matrix : ndarray
-                Decision matrix / alternatives data.
-                Alternatives are in rows and Criteria are in columns.
+            Parameters
+            ----------
+                matrix : ndarray
+                    Decision matrix / alternatives data.
+                    Alternatives are in rows and Criteria are in columns.
 
-            weights : ndarray
-                Criteria weights. Sum of the weights should be 1. (e.g. sum(weights) == 1)
+                weights : ndarray
+                    Criteria weights. Sum of the weights should be 1. (e.g.
+                    sum(weights) == 1)
 
-            types : ndarray
-                Array with definitions of criteria types:
-                1 if criteria is profit and -1 if criteria is cost for each criteria in `matrix`.
+                types : ndarray
+                    Array with definitions of criteria types:
+                    1 if criteria is profit and -1 if criteria is cost for 
+                    each criteria in `matrix`.
 
-            *args: is necessary for methods which reqiure some additional data.
+                skip_validations : bool
+                    Skip all the validations made when alternatives
+                    are evaluating. Default is False.
 
-            **kwargs: is necessary for methods which reqiure some additional data.
+                explained_call : bool
+                    Explain the MCDA, i.e. provide matrices and vectors from
+                    all the steps of the method, instead of return just the
+                    preference vector. Default is False.
         """
-        pass
+        matrix = np.array(matrix)
+        weights = np.array(weights)
+        types = np.array(types)
+        if not skip_validations:
+            self._validate_input_data(matrix, weights, types)
+
+        if explained_call:
+            return self._method_explained(matrix, weights, types)
+        else:
+            return self._method(matrix, weights, types)[-1]
 
     @staticmethod
     def _validate_input_data(matrix, weights, types):
-        if matrix.shape[1] != weights.shape[0] and weights.shape[0] != len(types):
-            raise ValueError(f'Number of criteria should be same as number of weights and number of types')
+        if (matrix.shape[1] != weights.shape[0]
+            or weights.shape[0] != len(types)):
+            raise ValueError('Number of criteria should be same as number of '
+                             'weights and number of types')
+
+        if abs(weights.sum() - 1) >= 0.01 or np.any(weights <= 0):
+            raise ValueError('Weights should be positive and its sum should '
+                             'be equal one. Now, sum of the weights is '
+                             f'{weights.sum()}.')
+
+        if np.sum(np.abs(types)) != types.shape[0]:
+            raise ValueError('Types array should only contains values -1 '
+                             'or 1.')
+
 
     def rank(self, a):
-        return rankdata(a, reverse=self.reverse_ranking)
+        return rankdata(a, reverse=self._reverse_ranking)
 
