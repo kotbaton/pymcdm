67261422ccfb4753940ef372d47779cd27347753
Andrii Shekhovtsov <zzzadey@gmail.com>
2023-08-11 10:09:29 +0200

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pymcdm",
-    version="1.1.1",
+    version="1.2.0",
     author="Andrii Shekhovtsov, Bartłomiej Kizielewicz",
     author_email="andrii-shekhovtsov@zut.edu.pl, bartlomiej-kizielewicz@zut.edu.pl",
     description="Python library for Multi-Criteria Decision-Making",
