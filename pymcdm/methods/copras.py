5497b39bc42705cf74b65b405b0a36ae4c0c3f65
Andrii Shekhovtsov <zzzadey@gmail.com>
2023-08-23 18:13:13 +0200

    Rewrite COPRAS method

--- pymcdm/methods/copras.py
+++ pymcdm/methods/copras.py
@@ -33,51 +33,29 @@ class COPRAS(MCDA_method):
         >>> [round(preference, 4) for preference in body(matrix, weights, types)]
         [1, 0.9167, 0.8675, 0.9084, 0.9315, 0.9486]
     """
+    _captions = [
+        'Normalized decision matrix.',
+        'Weighted normalized decision matrix.',
+        'Sum of maximising indices.',
+        'Sum of minimising indices.',
+        'Related significance.',
+        'Utility degree.'
+    ]
 
-    def __init__(self):
-        pass
-
-    def __call__(self, matrix, weights, types, *args, **kwargs):
-        """Rank alternatives from decision matrix `matrix`, with criteria weights `weights` and criteria types `types`.
-
-            Parameters
-            ----------
-                matrix : ndarray
-                    Decision matrix / alternatives data.
-                    Alternatives are in rows and Criteria are in columns.
-
-                weights : ndarray
-                    Criteria weights. Sum of the weights should be 1. (e.g. sum(weights) == 1)
-
-                types : ndarray
-                    Array with definitions of criteria types:
-                    1 if criteria is profit and -1 if criteria is cost for each criteria in `matrix`.
-
-                *args: is necessary for methods which reqiure some additional data.
-
-                **kwargs: is necessary for methods which reqiure some additional data.
-
-            Returns
-            -------
-                ndarray
-                    Preference values for alternatives. Better alternatives have higher values.
-        """
-        COPRAS._validate_input_data(matrix, weights, types)
+    def _method(self, matrix, weights, types):
         if np.all(types == 1.0):
-            raise ValueError('types array contains only profit criteria. COPRAS method requires at least one cost '
-                             'criteria.')
-        return COPRAS._copras(matrix, weights, types)
+            raise ValueError('types array contains only profit criteria.'
+                             ' COPRAS method requires at least one cost'
+                             ' criterion.')
 
-    @staticmethod
-    def _copras(matrix, weights, criteria_types):
         nmatrix = matrix / np.sum(matrix, axis=0)
 
         # Difficult normalized decision making matrix
         wmatrix = nmatrix * weights
 
-        Sp = np.sum(wmatrix[:, criteria_types == 1], axis=1)
-        Sm = np.sum(wmatrix[:, criteria_types == -1], axis=1)
+        Sp = np.sum(wmatrix[:, types == 1], axis=1)
+        Sm = np.sum(wmatrix[:, types == -1], axis=1)
 
         Q = Sp + ((np.min(Sm) * np.sum(Sm)) / (Sm * np.sum(np.min(Sm) / Sm)))
 
-        return Q / np.max(Q)
+        return (nmatrix, wmatrix, Sp, Sm, Q, Q / np.max(Q))
